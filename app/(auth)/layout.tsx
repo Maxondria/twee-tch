1b8a0504ec7a14191434f9d13b5b1bd39e4b09ba
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-02-06 11:12:35 +0300

    logo for the auth layout and styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,12 @@
 import { FC, PropsWithChildren } from "react";
+import { Logo } from "./_components/logo";
 
 const AuthLayout: FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
-    <main className="h-full flex items-center justify-center">{children}</main>
+    <main className="h-full flex flex-col items-center justify-center space-y-6">
+      <Logo />
+      {children}
+    </main>
   );
 };
 
